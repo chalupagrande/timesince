69432e5243ba7694fb21d6d402ef32d19be9c2ef
Jamie Skinner <jamie@ninthmind.com>
2016-05-20 16:28:11 -0500

    basic app built -- need to connect DB

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-// const http = require('http');
+'use strict'
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -12,18 +13,89 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-//POST
-app.post('/', (req, res, next)=>{
-  console.log('body '+ JSON.stringify(req.body))
-  res.send(req.body)
-})
+var storage = {}
 
-//GET
-app.get('/', (req, res, next)=>{
-  res.send('hello world')
+//POST
+app.post('/slack', (req, res, next)=>{
+  var b = req.body
+  if(b.token != "S83RYJu8eJcmqjxjEGFcbreJ"){
+    res.send("You have the wrong credentials. ")
+  }
+
+  var text = b.text
+  var command = text.split(' ')[0]
+  var name = text.slice(command.length+1)
+  var key = formatKey(name)
+
+  if(command == 'set' && !storage[key]){
+    storage[key] = {
+                     name: name,
+                     time: new Date()
+                   }
+    res.send(name + " has been created!")
+  } else if( command == "set" && storage[key]){
+    res.send(name +" alread exists!")
+  } else if( command == "reset" && storage[key]){
+    var time = findTime(storage[key].time)
+    storage[key].time = new Date()
+    res.send( name + " has been reset. It was at "+ time)
+  } else if( command == "reset" && !storage[key]){
+    res.send("There was no command found by the name: "+ name)
+  } else if( command == "get" && storage[key]){
+    var time = findTime(storage[key].time)
+    res.send("Time Since "+ name +": "+ time)
+  } else {
+    res.send('Something went wrong')
+  }
 })
 
 app.listen(port)
 console.log('running on '+ port)
 
-// const httpServer = http.createServer(app).listen(port)
+
+
+/* HELPERS
+~~~~~~~~~~~~~~~~~~~ */
+function findTime(date){
+  var now = new Date()
+  var diff = now - date
+
+  return formatTime(diff)
+}
+
+function formatTime(milliseconds) {
+   var s = Math.floor(milliseconds / 1000)
+   var m=0, h=0, d=0;
+
+   if(s / 60 >= 1){
+     m = Math.floor(s / 60)
+     s = s % 60
+   }
+   if(m / 60 >= 1){
+     h = Math.floor(m / 60)
+     m = m % 60
+   }
+   if(h / 24 >= 1){
+     d = Math.floor(h/24)
+     h = h % 24
+   }
+
+
+   var result = s + " seconds"
+   if(m) result = `${m} minutes ${s} seconds`
+   if(h) result = `${h} hours ${m} minutes ${s} seconds`
+   if(d) result = `${d} days ${h} hours ${m} minutes ${s} seconds`
+   return result
+}
+
+function formatObject(obj){
+  var result = {}
+  result.name = obj.name
+  result.time = new Date()
+  return result
+}
+
+function formatKey(string){
+  var temp = string.toLowerCase()
+  return temp.split(' ').join('-')
+}
